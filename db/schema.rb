f23706abfa66e4905dc210f74957cf7966b1c1b6
Rey David Cuevas <cuevas.vela@gmail.com>
2020-07-13 12:50:15 -0500

    changed db driver from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_12_023838) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "photos", force: :cascade do |t|
     t.text "title"
     t.text "description"
